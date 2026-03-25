34e3e9eecf91d659b57059e51efe88cf2998da71
Manuel Ferraz <alejandroofg@gmail.com>
2026-03-25 13:40:04 -0300

    Ajuste final de base path con slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://manuuafg.github.io', // Tu URL de GitHub Pages
-  base: '/portafolio', // Si tu repo se llama exactamente manuuafg.github.io, usa '/'
+  base: '/portafolio/', // Si tu repo se llama exactamente manuuafg.github.io, usa '/'
 });
